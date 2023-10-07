e293a833169733f75ca7e6b461545871d5cb55fb
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2023-10-07 10:49:09 +0300

    step complete

--- EFCore_Activity0201/Program.cs
+++ EFCore_Activity0201/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace EFCore_Activity0201
+﻿using EFCore_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_Activity0201
 {
     internal class Program
     {
+        private static IConfigurationRoot? _configuration;
+        private static DbContextOptionsBuilder<AdventureWorks2022Context>? _optionsBuilder;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BuildConfiguration();
+           // Console.WriteLine($"CNSTR: {_configuration.GetConnectionString("AdventureWorks")}");
+            BuildOptions();
+            ListPeople();
+        }
+        static void BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _configuration = builder.Build();
+        }
+        static void BuildOptions()
+        {
+            _optionsBuilder = new DbContextOptionsBuilder<AdventureWorks2022Context>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+        }
+        static void ListPeople()
+        {
+            using (var db = new AdventureWorks2022Context(_optionsBuilder.Options))
+            {
+                var people = db.People.OrderByDescending(x => x.LastName).Take(20).ToList();
+                foreach (var person in people)
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                }
+            }
         }
     }
 }
